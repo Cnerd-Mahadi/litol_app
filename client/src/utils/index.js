fd2e777431432a3a0d38504bdd789cae0c4c745a
Cnerd Mahadi <52869547+Cnerd-Mahadi@users.noreply.github.com>
2023-11-26 19:09:50 +0600

    Update index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -13,7 +13,7 @@ export const localUserData = () => {
 	return getLocalData("user");
 };
 
-export const baseURL = "https://litolapi-production.up.railway.app/api/";
+export const baseURL = "https://litolapp-production.up.railway.app/api/";
 // "http://127.0.0.1:8000/api/";
 
 export const queryClient = new QueryClient();
